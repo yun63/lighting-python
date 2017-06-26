95e551c897ed14ddc03a44a24f842d78f185fce1
=leiyunfei <tuyoo@tuyoodeMac-mini-10.local>
2017-06-26 15:23:00 +0800

    core

---
+++ base/lt_crypto.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+
+####################################################################################
+#
+# Copyright © 2017 TU. All Rights Reserved.
+#
+####################################################################################
+
+"""
+
+@File: lt_crypto.py
+
+@Description: 加密算法
+
+@Author: leiyunfei(leiyunfei@tuyoogame.com)
+
+@Depart: 棋牌中心
+
+@Create: 2017-06-21 17:43:01
+
+"""
+
+
+class Crypto(object):
+    pass
