e94e3814121f980e0e3aa04d8cd3c2593f4aa9a4
=leiyunfei <tuyoo@tuyoodeMac-mini-10.local>
2017-07-19 21:35:41 +0800

    按概率随机选择多个不重复的项

--- util/random_util.py
+++ util/random_util.py
@@ -28,13 +28,12 @@ def pick_by_weight(items):
     assert(isinstance(items, list))
     """
     根据权重概率随机（万分比)
-    param: items [{"item": {}, "p": 2000}, {"item": {}, "p": 4000}]
+    param: items [{..., "p": 2000}, {..., "p": 4000}]
     """
     total = sum([item.get('p', 0) for item in items])
-    print total
     base = random.randint(0, int(total))
     curtotal = 0
-    index = None
+    index = 0 
     for i, item in enumerate(items):
         print i, item
         curtotal = curtotal + item.get('p', 0)
@@ -43,14 +42,29 @@ def pick_by_weight(items):
             break
     picked_item = items[index]
     print 'base: %d, sum: %d, index: %d' % (base, total, index)
-    return picked_item.get('item', {})
+    return picked_item
+
+def getNoDuplicateItems(items, n):
+    assert(isinstance(items, list) and n <= len(items))
+    result = []
+    candidates = items[:]
+    while n > 0:
+        selected = pick_by_weight(candidates)
+        candidates.remove(selected)
+        result.append(selected)
+        n -= 1
+    return result
 
 
 if __name__ == '__main__':
     items = [
-        {'item': {'lei': 100, 'yun': 200, 'fei': 300}, 'p': 4000},
-        {'item': {'lei': 1000, 'yun': 2000, 'fei': 3000}, 'p': 3000},
-        {'item': {'lei': 10000, 'yun': 20000, 'fei': 30000}, 'p': 5000}
+        {'1': 100, 'p': 1000},
+        {'2': 200, 'p': 2000},
+        {'3': 300, 'p': 3000},
+        {'4': 400, 'p': 4000},
+        {'5': 500, 'p': 5000},
+        {'6': 600, 'p': 6000}
     ]
-    item = pick_by_weight(items)
-    print item
+    #item = pick_by_weight(items)
+    #print item
+    print getNoDuplicateItems(items, 2)
